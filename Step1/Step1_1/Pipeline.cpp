8d90eed3dec7d403bf967ca8270a664cae32cef9
Dendlian <86787641+Dendlian@users.noreply.github.com>
2021-10-20 23:37:55 +0900

    1

--- Step1/Step1_1/Pipeline.cpp
+++ Step1/Step1_1/Pipeline.cpp
@@ -9,6 +9,8 @@
 */
 
 #include <d3d11.h>
+#include <cassert>
+
 namespace Pipeline
 {
 	namespace
@@ -16,8 +18,11 @@ namespace Pipeline
 		ID3D11Device				* Device;				// 자원을 만드는 장소
 		ID3D11DeviceContext			* DeviceContext;		// 랜더링 파이프라인의 단계
 		IDXGISwapChain				* SwapChain;			// 나눠진 작업을 교대하며 실행
+															// 위 두 구조체의 디스크립션을 포함
 		ID3D11RenderTargetView		* RenderTargetView;		// 자원을 그리는 장소
 	}
+	
+
 
 	LRESULT CALLBACK Procedure(HWND const hWindow, UINT const uMessage, WPARAM const wParameter, LPARAM const lParameter)
 	{
@@ -25,6 +30,21 @@ namespace Pipeline
 		{
 			case WM_CREATE:
 			{
+				DXGI_SWAP_CHAIN_DESC Descriptor = DXGI_SWAP_CHAIN_DESC( );
+				Descriptor.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+				
+
+				Descriptor.SampleDesc.Count = 1;
+				// 픽셀의 단점(계단식 이미지)을 개선 : 안티앨리어싱
+				
+				Descriptor.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+				// 렌더링 타겟을 출력하는 용도로 사용
+
+				Descriptor.BufferCount = 1;
+				// 버퍼의 사용개수
+				// 0 : 버퍼 사용 X
+				// 1 : 버퍼 2개 사용
+
 				return 0;
 			}
 			case WM_APP:
