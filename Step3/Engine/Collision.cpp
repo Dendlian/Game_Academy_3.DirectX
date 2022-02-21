ffb1d25e046695e574b4ccd3e905ac7b1342dace
Dendlian <86787641+Dendlian@users.noreply.github.com>
2022-02-21 22:15:37 +0900

    4

--- Step3/Engine/Collision.cpp
+++ Step3/Engine/Collision.cpp
@@ -14,8 +14,8 @@ namespace Collision
             +cos(angle * radian), +sin(angle * radian),  
             -sin(angle * radian), +cos(angle * radian)
         };
-    } 
-
+    }
+     
 #pragma region Point vs Point
     bool Collide(Point const& LHS, Point const& RHS) 
     {
